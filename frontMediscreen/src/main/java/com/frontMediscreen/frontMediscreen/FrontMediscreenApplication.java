460fa96d7b2cffdc7f45993718e1ecd06094f472
Karl Gavillot <ksgavillot@hotmail.com>
2023-01-30 17:48:33 +0100

    first commit

---
+++ frontMediscreen/src/main/java/com/frontMediscreen/frontMediscreen/FrontMediscreenApplication.java
@@ -0,0 +1,13 @@
+package com.frontMediscreen.frontMediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontMediscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontMediscreenApplication.class, args);
+	}
+
+}
---
+++ frontMediscreen/src/test/java/com/frontMediscreen/frontMediscreen/FrontMediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontMediscreen.frontMediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontMediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
