8f0ae2f9de083eddf44fb6e82d0a2dbacc8ff23c
Karl Gavillot <ksgavillot@hotmail.com>
2023-02-03 16:38:41 +0100

    UI create patient sur frontMicroservice

--- frontMediscreen/src/main/java/com/frontMediscreen/frontMediscreen/controller/PatientController.java
+++ frontMediscreen/src/main/java/com/frontMediscreen/frontMediscreen/controller/PatientController.java
@@ -7,9 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PatientController {
@@ -21,10 +24,23 @@ public class PatientController {
 
     @GetMapping("/patient/list")
     public String listAllPatients(Model model) {
-        logger.info("Get patient list");
+        logger.info("Get patient list from frontMicroservice");
         List<PatientBean> listPatient = patientProxy.listAllPatients();
         model.addAttribute("patients", listPatient);
         return "patient/listPatient";
+    }
+
+    @GetMapping("/patient/newForm")
+    public String showNewForm(Model model) {
+        model.addAttribute("patient", new PatientBean());
+        model.addAttribute("pageTitle", "Add New Patient");
+        return "patient/patient_form";
+    }
+
+    @PostMapping("/patient/add")
+    public String addPatient(PatientBean patient, RedirectAttributes redirectAttributes) {
+        patientProxy.addPatient(patient);
+        return "redirect:/patient/list";
     }
 
 
