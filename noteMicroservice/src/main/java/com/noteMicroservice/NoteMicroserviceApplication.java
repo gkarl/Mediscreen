7e93accc4faee72e86cbd721c74510c321181384
Karl Gavillot <ksgavillot@hotmail.com>
2023-02-08 14:15:31 +0100

    first commit note microservice

---
+++ noteMicroservice/src/main/java/com/noteMicroservice/NoteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.noteMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteMicroserviceApplication.class, args);
+	}
+
+}
