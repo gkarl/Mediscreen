0813fba7dfac00e49094809517853e946893d646
Karl Gavillot <ksgavillot@hotmail.com>
2023-02-09 17:29:05 +0100

    connect note microservice avec front microservice pour read opérations

---
+++ frontMediscreen/src/main/java/com/frontMediscreen/frontMediscreen/beans/NoteBean.java
@@ -0,0 +1,58 @@
+package com.frontMediscreen.frontMediscreen.beans;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+public class NoteBean {
+
+    private String id;
+
+    private Integer patientId;
+
+    private LocalDate date;
+
+    @NotBlank
+    private String recommendation;
+
+    public NoteBean() {
+    }
+
+    public NoteBean(String id, Integer patientId, LocalDate date, String recommendation) {
+        this.id = id;
+        this.patientId = patientId;
+        this.date = date;
+        this.recommendation = recommendation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getRecommendation() {
+        return recommendation;
+    }
+
+    public void setRecommendation(String recommendation) {
+        this.recommendation = recommendation;
+    }
+}
---
+++ frontMediscreen/src/main/java/com/frontMediscreen/frontMediscreen/proxies/NoteProxy.java
@@ -0,0 +1,18 @@
+package com.frontMediscreen.frontMediscreen.proxies;
+
+import com.frontMediscreen.frontMediscreen.beans.NoteBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "Note-microservice", url = "localhost:8082")
+public interface NoteProxy {
+
+    @GetMapping("/note/{id}")
+    NoteBean getNoteById(@PathVariable("id") String id);
+
+    @GetMapping("/note/list/{patientId}")
+    List<NoteBean> listNoteByPatientId(@PathVariable ("patientId") Integer patientId);
+}
